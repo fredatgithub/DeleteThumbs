0155d872607ef1827eb3d46b659d2e40b95314f4
Fred <domaine75@gmail.com>
2020-04-04 13:22:31 +0200

    Add a method to list all directories and subdirectories

--- DeleteThumbs/Program.cs
+++ DeleteThumbs/Program.cs
@@ -14,40 +14,43 @@ namespace DeleteThumbs
       var listeFichierEffaces = new List<string>();
       Display("Searching for all thumbs.db file:");
       int fileCount = 0;
-      foreach (var searchFile in SearchFiles(modele))
-      {
-        fileCount++;
-        if (searchFile != null)
-        {
-          Display(searchFile.ToString());
-          if (searchFile.DirectoryName != null)
-          {
-            Display(Path.Combine(searchFile.DirectoryName, searchFile.Name));
-            listeFichierEffaces.Add(Path.Combine(searchFile.DirectoryName, searchFile.Name));
-            try
-            {
-              File.Delete(string.Format(Path.Combine(searchFile.DirectoryName, searchFile.Name)));
-            }
-            catch (Exception exception)
-            {
-              Display($"There was an exception {exception.Message}");
-            }
-          }
-        }
-      }
+      var directories = CustomSearcher.GetDirectories("c:\\");
 
-      string listDrives = string.Empty;
-      foreach (
-          DriveInfo drive in
-              DriveInfo.GetDrives()
-                  .Where(drive => drive.DriveType != DriveType.CDRom)
-                  .Where(drive => drive.DriveType != DriveType.Network)
-                  .Where(drive => drive.DriveType != DriveType.Removable))
-      {
-        listDrives += $"{drive.Name} ";
-      }
+      //foreach (var searchFile in SearchFiles(modele))
+      //{
+      //  fileCount++;
+      //  if (searchFile != null)
+      //  {
+      //    Display(searchFile.ToString());
+      //    if (searchFile.DirectoryName != null)
+      //    {
+      //      Display(Path.Combine(searchFile.DirectoryName, searchFile.Name));
+      //      listeFichierEffaces.Add(Path.Combine(searchFile.DirectoryName, searchFile.Name));
+      //      try
+      //      {
+      //        File.Delete(string.Format(Path.Combine(searchFile.DirectoryName, searchFile.Name)));
+      //      }
+      //      catch (Exception exception)
+      //      {
+      //        Display($"There was an exception {exception.Message}");
+      //      }
+      //    }
+      //  }
+      //}
 
-      Display($"list of drives: {listDrives}");
+      //string listDrives = string.Empty;
+      //foreach (
+      //    DriveInfo drive in
+      //        DriveInfo.GetDrives()
+      //            .Where(drive => drive.DriveType != DriveType.CDRom)
+      //            .Where(drive => drive.DriveType != DriveType.Network)
+      //            .Where(drive => drive.DriveType != DriveType.Removable))
+      //{
+      //  listDrives += $"{drive.Name} ";
+      //}
+
+      //Display($"list of drives: {listDrives}");
+      Display($"the number of directories found is {directories.Count}");
       Display($"Number of files schecked: {fileCount}");
       Display("Press a key to exit :");
       Console.ReadKey();
